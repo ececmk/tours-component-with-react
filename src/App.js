53fd9be08a1f094ecc50f87e62c061a6727828d8
Ece Çakmak <86318154+ececmk@users.noreply.github.com>
2023-01-18 16:13:02 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, {useState, useEffect} from 'react';
+import Loading from './Loading';
+import Tours from './Tours';
+import './App.css';
+
+const url = 'https://course-api.com/react-tours-project'
+
+function App() {
+  return (
+    <div>
+      Tours 
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Tour.js
@@ -0,0 +1,10 @@
+function Tour () {
+
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+export default Tour;
